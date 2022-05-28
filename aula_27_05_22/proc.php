9b8534dc8dd141b845542defebff241e1394b35f
Kewyn Willyam Souza Torres <79849103+projetos-etec-kewyn@users.noreply.github.com>
2022-05-28 11:29:52 -0300

    Add files via upload

--- aula_27_05_22/proc.php
+++ aula_27_05_22/proc.php
@@ -1,8 +1,12 @@
 <?php
 $n = $_POST['num'];
-function imp($num){
-    for($i=1;$i<$num;$i++){
-        
+function imprimir($num){
+    for($i=1;$i<=$num;$i++){
+        for($a=1;$a<=$i;$a++){
+            echo "$a  ";
+        }
+        echo "<br>";
     }
 }
-?>
+imprimir($n);
+?> 
